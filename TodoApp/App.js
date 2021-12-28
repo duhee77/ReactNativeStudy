2c4cb243e22c4660fe67820549060c7517cf2ab9
duhee77 <dwd4798@gmail.com>
2021-12-28 12:08:50 +0900

    Add Layout

--- TodoApp/App.js
+++ TodoApp/App.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import DateHead from './components/DateHead';
+import AddTodo from './components/AddTodo';
+import Empty from './components/Empty';
 import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 
 function App() {
   const today = new Date();
   return (
     <SafeAreaProvider>
-      <SafeAreaView edges={['bottom']}>
+      <SafeAreaView edges={['bottom']} style={styles.block}>
         <DateHead date={today} />
+        <Empty />
+        <AddTodo />
       </SafeAreaView>
     </SafeAreaProvider>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+});
 
 export default App;
