0c8fb0863c0d5fada74b26a1c3dc34630140be83
duhee77 <dwd4798@gmail.com>
2021-12-29 19:41:52 +0900

    Add storage

--- TodoApp/App.js
+++ TodoApp/App.js
@@ -1,12 +1,60 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, KeyboardAvoidingView, Platform} from 'react-native';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+
 import DateHead from './components/DateHead';
 import AddTodo from './components/AddTodo';
 import Empty from './components/Empty';
-import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+import TodoList from './components/TodoList';
+import todosStorage from './components/todosStorage';
 
 function App() {
   const today = new Date();
+
+  const [todos, setTodos] = useState([
+    {id: 1, text: '+ 버튼을 눌러 todo를 추가하실 수 있습니다.', done: false},
+    {id: 2, text: '체크를 통해 끝난 일을 체크하실 수 있습니다.', done: false},
+    {
+      id: 3,
+      text: '끝난 일을 삭제하고 싶다면, 우측 아이콘을 눌러주세요.',
+      done: true,
+    },
+  ]);
+
+  // load: if App mounted/unmounted
+  // App never unmounted before Application quit
+  useEffect(() => {
+    todosStorage.get().then(setTodos).catch(console.error);
+  }, []);
+
+  // save: if todos changed
+  useEffect(() => {
+    todosStorage.set(todos).catch(console.error);
+  }, [todos]);
+
+  const onInsert = text => {
+    const nextId =
+      todos.length > 0 ? Math.max(...todos.map(todo => todo.id)) + 1 : 1;
+    const todo = {
+      id: nextId,
+      text,
+      done: false,
+    };
+    setTodos(todos.concat(todo));
+  };
+
+  const onToggle = id => {
+    const nextTodos = todos.map(todo =>
+      todo.id === id ? {...todo, done: !todo.done} : todo,
+    );
+    setTodos(nextTodos);
+  };
+
+  const onRemove = id => {
+    const nextTodos = todos.filter(todo => todo.id != id);
+    setTodos(nextTodos);
+  };
+
   return (
     <SafeAreaProvider>
       <SafeAreaView edges={['bottom']} style={styles.block}>
@@ -14,8 +62,12 @@ function App() {
           behavior={Platform.select({ios: 'padding'})}
           style={styles.avoid}>
           <DateHead date={today} />
-          <Empty />
-          <AddTodo />
+          {todos.length === 0 ? (
+            <Empty />
+          ) : (
+            <TodoList todos={todos} onToggle={onToggle} onRemove={onRemove} />
+          )}
+          <AddTodo onInsert={onInsert} />
         </KeyboardAvoidingView>
       </SafeAreaView>
     </SafeAreaProvider>
