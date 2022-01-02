e65a7059459bfddc7c27c981e15e5bf983aadf27
duhee77 <dwd4798@gmail.com>
2022-01-02 23:40:44 +0900

    Edit Drawer

--- Chap5_AnotherNavigator/App.js
+++ Chap5_AnotherNavigator/App.js
@@ -4,7 +4,6 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
-
 const Drawer = createDrawerNavigator();
 
 function HomeScreen({navigation}) {
@@ -12,7 +11,7 @@ function HomeScreen({navigation}) {
     <View>
       <Text>Home</Text>
       <Button title="Drawer 열기" onPress={() => navigation.openDrawer()} />
-      <Button 
+      <Button
         title="Setting 열기"
         onPress={() => navigation.navigate('Setting')}
       />
@@ -24,7 +23,7 @@ function SettingScreen({navigation}) {
   return (
     <View>
       <Text>Setting</Text>
-      <Button title="뒤로가기" onPress={()=> navigation.goBack()} />
+      <Button title="뒤로가기" onPress={() => navigation.goBack()} />
     </View>
   );
 }
@@ -36,18 +35,20 @@ function App() {
         initialRouteName="Home"
         drawerPosition="left"
         backBehavior="history"
-        drawerContent={({navigation}) => (
-          <SafeAreaView>
-            <Text>A Custom Drawer</Text>
-            <Button
-              onPress={()=> navigation.closeDrawer()}
-              title="Drawer 닫기"
-            />
-          </SafeAreaView>
-        )}
-      >
-        <Drawer.Screen name="Home" component={HomeScreen} options={{title: '홈'}}/>
-        <Drawer.Screen name="Setting" component={SettingScreen} options={{title: '설정'}}/>
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
+        <Drawer.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: '홈', headerLeft: () => <Text>Left</Text>}}
+        />
+        <Drawer.Screen
+          name="Setting"
+          component={SettingScreen}
+          options={{title: '설정'}}
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
