0a988527e4edc0c8224892650205684eaf656868
duhee77 <dwd4798@gmail.com>
2022-01-11 23:31:50 +0900

    6.2 Context API 사용하기

--- Chap6_DayLog/screens/FeedsScreen.js
+++ Chap6_DayLog/screens/FeedsScreen.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
+import LogContext from '../contexts/LogContext';
+import {Text} from 'react-native';
 
 const FeedsScreen = () => {
-  return <View style={styles.block} />;
+  return (
+    <View style={styles.block}>
+      <LogContext.Consumer>{value => <Text>{value}</Text>}</LogContext.Consumer>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
